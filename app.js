1d655c8b82436686ee60697d39a78b8f3ef0cbdf
Alex Cardenas <alexandercardenas50x@gmail.com>
2026-03-03 11:01:43 -0500

    Se agrego la funcion de contar platos

--- app.js
+++ app.js
@@ -2,20 +2,32 @@
 let menu = [
     { nombre: "Arroz con pollo", precio: 12, stock: 5 },
     { nombre: "Lomo saltado", precio: 18, stock: 3 },
-    { nombre: "Sopa", precio: 8, stock: 10 }
+    { nombre: "Sopa", precio: 8, stock: 10 },
+
 ];
+
+function ContarPlatos(menux) {
+    let total = 0;
+    total = menux.length;
+    return total;
+}
 
 // 2) FUNCIÓN: renderizar (mostrar) el menú en pantalla
 function renderMenu() {
     const output = document.getElementById("output");
     output.innerHTML = ""; // limpiar
 
+    let total = ContarPlatos(menu)
+    let html = "<h1>Platos disponibles:" + total + "</h1>"
+
     // crear una lista HTML simple
-    let html = "<ul>";
+    html += "<ul>";
 
     for (let i = 0; i < menu.length; i++) {
         const plato = menu[i];
         html += `<li>${plato.nombre} — S/ ${plato.precio} — Stock: ${plato.stock}</li>`;
+        let precio = 1
+        precio += 2
     }
 
     html += "</ul>";
@@ -24,7 +36,7 @@ function renderMenu() {
 
 // 3) FUNCIÓN: agregar un plato demo al menú
 function agregarPlatoDemo() {
-    const nuevoPlato = { nombre: "Pollo a la brasa", precio: 20, stock: 4 };
+    const nuevoPlato = { nombre: "pollo a la brasa", precio: 20, stock: 4 };
     menu.push(nuevoPlato);
 }
 
@@ -37,3 +49,7 @@ document.getElementById("btnAgregar").addEventListener("click", () => {
     agregarPlatoDemo();
     renderMenu();
 });
+
+
+
+ContarPlatos(menu)
