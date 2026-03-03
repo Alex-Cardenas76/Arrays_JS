25c26db3bfb9e35cfd5e89563e7c74f34c264044
Alex Cardenas <alexandercardenas50x@gmail.com>
2026-03-03 11:02:17 -0500

    Se agregaron 2 nuevos platos al menu

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let menu = [
     { nombre: "Arroz con pollo", precio: 12, stock: 5 },
     { nombre: "Lomo saltado", precio: 18, stock: 3 },
     { nombre: "Sopa", precio: 8, stock: 10 },
+    { nombre: "ceviche", precio: 10, stock: 6 },
+    { nombre: "chicharron", precio: 15, stock: 4 }
 
 ];
 
