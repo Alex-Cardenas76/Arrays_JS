f206a07d9f7b1697e009d946af1fa3a17cab4358
Alex Cardenas <alexandercardenas50x@gmail.com>
2026-03-05 11:46:51 -0500

    Incorporacion de clases a render menu por estado de stock , parte D culminada

--- app.js
+++ app.js
@@ -31,13 +31,16 @@ function renderMenu() {
         const plato = menu[i];
         let estado = " "
         if (plato.stock == 0) {
-            estado = "❌agotado";
+            estado = "agotado";
+
         } else if (plato.stock <= 3) {
-            estado = "⚠️bajo";
+            estado = "bajo";
+
         } else {
-            estado = "🟢normal";
+            estado = "normal";
+
         }
-        html += `<li>${plato.nombre} — S/ ${plato.precio} — Stock: ${plato.stock} - ${estado}</li>`;
+        html += `<li class="${estado}">${plato.nombre} — S/ ${plato.precio} — Stock: ${plato.stock} - ${estado}</li>`;
     }
 
     html += "</ul>";
