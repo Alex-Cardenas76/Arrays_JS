d6d84169395dce68c4dbdf5f28326936158f6dc3
luis enrique <luiscajacurigutierrezgmail.com>
2026-03-06 12:53:20 -0500

    main.js: orquestacion de la aplicacion, importacion de modulos y arranque del sistema

--- main.js
+++ main.js
@@ -1,24 +1,3 @@
-let menu = [
-    { nombre: "Arroz con pollo", precio: 12, stock: 7 },
-    { nombre: "Lomo saltado", precio: 18, stock: 3 },
-    { nombre: "Sopa", precio: 8, stock: 9 },
-    // 2 platos mas
-    { nombre: "chaufa", precio: 12, stock: 7 },
-    { nombre: "Aji de gallina", precio: 11, stock: 3 }
-
-];
-
-function agregarPlatoDemo() {
-    const nuevoPlato = { nombre: "Tallarin saltado", precio: 15, stock: 2 };
-    menu.push(nuevoPlato);
-}
-
-function actualizarStock(nombre, nuevoStock) { // checkear
- for (let i = 0; i < menu.length; i++) {
-   if (menu[i].nombre.toLowerCase() === nombre.toLowerCase()) {
-     menu[i].stock = nuevoStock;
-     return true;
-   }
- }
- return false;
-}
+import { conectarEventos, renderMenu } from "./ui.js";
+conectarEventos();
+renderMenu();
