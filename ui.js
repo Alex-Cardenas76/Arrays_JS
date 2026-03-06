85a435d9bccbb0bcbcc9830bb0a3ccc03a3ac7ec
luis enrique <luiscajacurigutierrezgmail.com>
2026-03-06 12:52:15 -0500

    ui.js: desarrollo de funciones para mostrar el menu en el DOM y vinculacion de eventos

---
+++ ui.js
@@ -0,0 +1,87 @@
+import { menu, agregarPlatoDemo } from "./menu.js";
+import { buscarPlatoPorNombre, filtrarPlatosStockBajo, obtenerResumenMenu, venderPlato } from "./operaciones.js";
+import { contarPlatos } from "./operaciones.js";
+
+// 2) FUNCIÓN: renderizar (mostrar) el menú en pantalla
+export function renderMenu() {
+    const output = document.getElementById("output");
+    output.innerHTML = ""; // limpiar
+    let html = "<h2>Total de platos en el menú: " + contarPlatos(menu) + "</h2>";
+
+
+    // crear una lista HTML simple
+    html += "<ul>";
+
+    for (let i = 0; i < menu.length; i++) {
+        const plato = menu[i];
+        let estado = " "
+        if (plato.stock == 0) {
+            estado = "agotado";
+
+        } else if (plato.stock <= 3) {
+            estado = "bajo";
+
+        } else {
+            estado = "normal";
+
+        }
+        html += `<li class="${estado}">${plato.nombre} — S/ ${plato.precio} — Stock: ${plato.stock} - ${estado}</li>`;
+    }
+
+    html += "</ul>";
+    output.innerHTML = html;
+    return html;
+}
+
+//PARTE C
+export function renderLista(titulo, listaDeTextos) {
+    const contenedor = document.getElementById("output");
+    contenedor.innerHTML = "";
+
+    let html = "<h2>" + titulo + "</h2>";
+    html += "<ul>";
+    for (let i = 0; i < listaDeTextos.length; i++) {
+        html += "<li>" + listaDeTextos[i] + "</li>";
+    }
+    html += "</ul>";
+    contenedor.innerHTML = html;
+}
+
+
+export function conectarEventos() {
+    // 4) EVENTOS: conectar botones con funciones
+    document.getElementById("btnMostrar").addEventListener("click", () => {
+        renderMenu();
+    });
+
+    document.getElementById("btnAgregar").addEventListener("click", () => {
+        agregarPlatoDemo();
+        renderMenu();
+    });
+
+    document.getElementById("btnResumen").addEventListener("click", () => {
+        obtenerResumenMenu();
+
+    });
+
+    document.getElementById("btnBuscar").addEventListener("click", () => {
+        buscarPlatoPorNombre();
+
+    });
+
+    document.getElementById("btnStockBajo").addEventListener("click", () => {
+        filtrarPlatosStockBajo();
+
+    });
+
+    document.getElementById("btnVender").addEventListener("click", () => {
+        let nombre = document.getElementById("inputBuscar").value
+        let cantidad = document.getElementById("inputCantidad").value
+        let mensaje = venderPlato(nombre, cantidad);
+        let tmenu = renderMenu();
+        document.getElementById("output").innerHTML = tmenu + mensaje;
+
+
+    });
+
+}
